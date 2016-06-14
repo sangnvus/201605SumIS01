15f8a2c35fb82936bcced3ce25c8f5228a1e8909
SE03451 <soulivongse03451@fpt.edu.vn>
2016-06-15 01:15:17 +0700

    update function to home page

--- WIP/User/AnGiProject/application/controllers/Home.php
+++ WIP/User/AnGiProject/application/controllers/Home.php
@@ -1,16 +1,44 @@
-<?php 
-if (!defined('BASEPATH')) exit('No direct script access allowed');
-  
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allow');
+
 class Home extends CI_Controller {
 
-	 public function __construct(){
+    public function __construct() {
         parent::__construct();
-         $this->load->helper('url');
-         $this->load->library('session');
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->model('Restaurants_model');
     }
-  
+
     public function index() {
+        
+        // array to store data
         $data = array();
+        
+        // --------------------- Top rate -------------------------
+        $query = $this->Restaurants_model->getTopRatingRestaurant();
+
+        // retrieve data from restaurant model
+        for ($i = 0; $i < 4; $i++) {
+            $data['restName' . $i] = $query[$i][1];
+            $data['address' . $i] = $query[$i][2];
+            $data['image' . $i] = $query[$i][3];
+            $data['rate' . $i] = $query[$i][4];
+        }
+        
+        // --------------------- End Top Rate -------------------------
+        
+        // --------------------- Top Promotion -------------------------
+        $data['img'] = $this->Restaurants_model->getTopPromotionRestaurant();
+        
+        
+        // --------------------- End Top Promotion -------------------------
+        
+        
+        
+        
+        // return to view
         $data['content'] = 'site/home/index/index.phtml';
         $this->load->view('site/layout/layout.phtml', $data);
     }
@@ -20,4 +48,5 @@ class Home extends CI_Controller {
         $data['content'] = 'site/home/search/index.phtml';
         $this->load->view('site/layout/layout.phtml', $data);
     }
+
 }
