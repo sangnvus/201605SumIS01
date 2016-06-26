d249224950e3be8a225daacdd759f588bf11819e
hieuvh <vuhoanghieu17894@gmail.com>
2016-06-26 14:51:08 -0400

    a

--- WIP/User/AnGiProject/application/controllers/Home.php
+++ WIP/User/AnGiProject/application/controllers/Home.php
@@ -1,38 +1,23 @@
-<?php
-
-defined('BASEPATH') OR exit('No direct script access allow');
-
+<?php 
+if (!defined('BASEPATH')) exit('No direct script access allowed');
+  
 class Home extends CI_Controller {
 
-    public function __construct() {
+	 public function __construct(){
         parent::__construct();
-        $this->load->database();
-        $this->load->helper('url');
-        $this->load->model('Restaurants_model');
+         $this->load->helper('url');
+         $this->load->library('session');
     }
-
+  
     public function index() {
-
-        // retrieve data from model
-        $rate = $this->Restaurants_model->getInterestingRestaurants('rate');
-        $discount = $this->Restaurants_model->getInterestingRestaurants('discount');
-
-        if ($this->Restaurants_model->getInterestingRestaurants('isReturn')) {
-            // prepare data to view
-            $data['rate'] = $rate;
-            $data['discount'] = $discount;
-            $data['limitRate'] = 4;
-            $data['limitDis'] = 8;
-
-            // return to view
-            $data['content'] = 'site/home/index/index.phtml';
-            $this->load->view('site/layout/layout.phtml', $data);
-        }else{
-            echo "No information exists in database!";
-            die();
-            $data['content'] = 'site/home/index/index.phtml';
-            $this->load->view('site/layout/layout.phtml', $data);
-        }
+        $data = array();
+        $data['content'] = 'site/home/index/index.phtml';
+        $this->load->view('site/layout/layout.phtml', $data);
     }
 
+    public function search() {
+        $data = array();
+        $data['content'] = 'site/home/search/index.phtml';
+        $this->load->view('site/layout/layout.phtml', $data);
+    }
 }
