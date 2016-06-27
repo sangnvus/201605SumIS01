eda1c46a791196204c730fb9c559b6db029f3b52
soulivongse03451 <soulivongse03451@fpt.edu.vn>
2016-06-27 15:03:59 +0700

    repair home controller

--- WIP/User/AnGiProject/application/controllers/Home.php
+++ WIP/User/AnGiProject/application/controllers/Home.php
@@ -10,9 +10,27 @@ class Home extends CI_Controller {
     }
   
     public function index() {
-        $data = array();
-        $data['content'] = 'site/home/index/index.phtml';
-        $this->load->view('site/layout/layout.phtml', $data);
+       // retrieve data from model
+        $rate = $this->Restaurants_model->getInterestingRestaurants('rate');
+        $discount = $this->Restaurants_model->getInterestingRestaurants('discount');
+
+        if ($this->Restaurants_model->getInterestingRestaurants('isReturn')) {
+            // prepare data to view
+            $data['rate'] = $rate;
+            $data['discount'] = $discount;
+            $data['limitRate'] = 4;
+            $data['limitDis'] = 8;
+
+            // return to view
+            $data['content'] = 'site/home/index/index.phtml';
+            $this->load->view('site/layout/layout.phtml', $data);
+        }else{
+       		// inform user that db does not exist, (NEED TO UPDATE UI)
+            echo "No information exists in database!";
+            die();
+            $data['content'] = 'site/home/index/index.phtml';
+            $this->load->view('site/layout/layout.phtml', $data);
+        }
     }
 
     public function search() {
