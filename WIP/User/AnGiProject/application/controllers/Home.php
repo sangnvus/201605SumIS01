a7874ed20fb6cd02a68613d155ad449fc4f950e3
soulivongse03451 <soulivongse03451@fpt.edu.vn>
2016-07-07 21:11:32 +0700

    re-update home controller

--- WIP/User/AnGiProject/application/controllers/Home.php
+++ WIP/User/AnGiProject/application/controllers/Home.php
@@ -1,16 +1,19 @@
-<?php 
-if (!defined('BASEPATH')) exit('No direct script access allowed');
-  
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allow');
+
 class Home extends CI_Controller {
 
-	 public function __construct(){
+    public function __construct() {
         parent::__construct();
-         $this->load->helper('url');
-         $this->load->library('session');
+        $this->load->database();
+        $this->load->helper(array('url', 'form'));
+        $this->load->model('Restaurants_model');
     }
-  
+
     public function index() {
-       // retrieve data from model
+
+        // retrieve data from model
         $rate = $this->Restaurants_model->getInterestingRestaurants('rate');
         $discount = $this->Restaurants_model->getInterestingRestaurants('discount');
 
@@ -25,17 +28,10 @@ class Home extends CI_Controller {
             $data['content'] = 'site/home/index/index.phtml';
             $this->load->view('site/layout/layout.phtml', $data);
         }else{
-       		// inform user that db does not exist, (NEED TO UPDATE UI)
             echo "No information exists in database!";
             die();
             $data['content'] = 'site/home/index/index.phtml';
             $this->load->view('site/layout/layout.phtml', $data);
         }
-    }
-
-    public function search() {
-        $data = array();
-        $data['content'] = 'site/home/search/index.phtml';
-        $this->load->view('site/layout/layout.phtml', $data);
     }
 }
