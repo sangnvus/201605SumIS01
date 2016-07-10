024cef3dd8f017137952b360812616e05d612a9f
soulivongse03451 <soulivongse03451@fpt.edu.vn>
2016-07-10 11:13:58 +0700

    change structure and link

--- WIP/User/AnGiProject/application/controllers/restaurant.php
+++ WIP/User/AnGiProject/application/controllers/restaurant.php
@@ -1,11 +1,13 @@
 <?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Restaurant extends CI_Controller {
 
-    public function __construct(){
+    public function __construct() {
         parent::__construct();
-        $this->load->helper('url');
+        $this->load->helper(array('url', 'form'));
         $this->load->library('session');
     }
 
@@ -15,15 +17,34 @@ class Restaurant extends CI_Controller {
         $this->load->view('site/layout/layout.phtml', $data);
     }
 
-    public function category(){
+    public function category() {
         $data = array();
         $data['content'] = 'site/restaurant/category.phtml';
         $this->load->view('site/layout/layout.phtml', $data);
     }
 
-    public function view(){
-        $data = array();
+    public function view($restUrl) {
+        $data['restUrl'] = $restUrl;
         $data['content'] = 'site/restaurant/view.phtml';
         $this->load->view('site/layout/layout.phtml', $data);
     }
+    
+    public function Restaurant_Banner() {
+        $data = array();
+        $data['content'] = 'site/restaurant/Rbanner.phtml';
+        $this->load->view('site/layout/layout.phtml', $data);
+    }
+
+    public function Restaurant_infor() {
+        $data = array();
+        $data['content'] = 'site/restaurant/Rinfor.phtml';
+        $this->load->view('site/layout/layout.phtml', $data);
+    }
+
+    public function Restaurant_price() {
+        $data = array();
+        $data['content'] = 'site/restaurant/Rprice.phtml';
+        $this->load->view('site/layout/layout.phtml', $data);
+    }
+    
 }
