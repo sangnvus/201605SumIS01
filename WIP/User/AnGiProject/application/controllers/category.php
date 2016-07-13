a811faef55296ef01758485891044e4e0f26f3c0
hoangnse03193 <hoangnse03193@fpt.edu.vn>
2016-07-13 13:52:34 +0700

    Commit lại Category do lần trước chưa lên

--- WIP/User/AnGiProject/application/controllers/category.php
+++ WIP/User/AnGiProject/application/controllers/category.php
@@ -10,22 +10,32 @@ class Category extends CI_Controller
 		$this->load->model('Category_model');
 	}
 	
-	function index($local= 0)
+	function index($type = 0,$ID = 0)
 	{
 		$data['content'] = 'site/restaurant/Category.phtml';
+		//echo "string".$type."ID =".$ID;
+		$districtData = $this->Category_model-> getDistrict();
+        $categoriesData = $this->Category_model-> getCategories();
+        //$resData = null;
 
-		$districtData = $this->Category_model-> getDistrict($local);
+        if($type != 2){
+        	$resData = $this->Category_model->getResByCate($ID);
+        }
+        else{
+        	$resData = $this->Category_model->getResByLocal($ID);
+        }
         
-        $resData = $this->Category_model->getRestaurant($local);
+        $rate = array();
         foreach ($resData as $key => $row)
         {
-        
-        	$rate = $this->Category_model->getRate($row['restaurantID']);
-        	print_r($rate);
+        	$rate[] = $this->Category_model->getRate($row['restaurantID']);
+
         }
+        //print_r($rate);       
+        $data['categoriesData'] = $categoriesData;
         $data['districtData'] = $districtData;
         $data['resData'] = $resData;
-        
+        $data['rate'] = $rate;
         $this->load->view('site/layout/layout.phtml',$data);
 
     }
--- WIP/User/AnGiProject/application/models/category_model.php
+++ WIP/User/AnGiProject/application/models/category_model.php
@@ -8,42 +8,74 @@ class Category_model extends CI_Model
     }
 	
 
-	function getRestaurant($local)
+    function getResByLocal($ID)
     {
-    	if ($local == 0){
-    		$this->db->select('*');
-		$this->db->from('restaurants r'); 
+        if ($ID == 0){
+            $this->db->select('*');
+        $this->db->from('restaurants r'); 
         $this->db->join('address a', 'r.addressID = a.addressID');
         $this->db->join('district d', 'd.districtID = a.districtID');
         $query = $this->db->get(); 
         $data=$query->result_array();
-   		return  $data;
-    	}
-    	else{
-			$this->db->select('*');
-			$this->db->from('restaurants r'); 
-		    $this->db->join('address a', 'r.addressID = a.addressID');
-		    $this->db->join('district d', 'd.districtID = a.districtID');
-		    $this->db->where('d.districtID',$local); 
-		    $query = $this->db->get(); 
-		    $data=$query->result_array();
-			return  $data;
-		}
-	}
+        return  $data;
+        }
+        else{
+            $this->db->select('*');
+            $this->db->from('restaurants r'); 
+            $this->db->join('address a', 'r.addressID = a.addressID');
+            $this->db->join('district d', 'd.districtID = a.districtID');
+            $this->db->where('d.districtID',$ID); 
+            $query = $this->db->get(); 
+            $data=$query->result_array();
+            return  $data;
+        }
+    }
+
+    function getResByCate($ID)
+    {
+        if ($ID == 0){
+        $this->db->select('*');
+        $this->db->from('restaurants r'); 
+        $this->db->join('address a', 'r.addressID = a.addressID');
+        $query = $this->db->get(); 
+        $data=$query->result_array();
+        return  $data;
+        }
+        else{
+            $this->db->select('*');
+            $this->db->from('restaurants r'); 
+            $this->db->join('address a', 'r.addressID = a.addressID');
+            $this->db->join('restaurantcategories rc', 'rc.restaurantID = r.restaurantID');
+            $this->db->where('rc.categoryOfResID',$ID); 
+            $query = $this->db->get(); 
+            $data=$query->result_array();
+            return  $data;
+        }
+    }
+
+
 
     function getRate($restaurantID){
         $this->db->select_avg('rateValue','overall');
         $this->db->from('rate');
         $this->db->where('restaurantID', $restaurantID);
         $data = $this->db->get()->row();
-        return  $data;
+        return  $data->overall;
     }
-	function getDistrict($table)
+
+	function getDistrict()
     {
 		$query = $this->db->get_where('district', array('provinceID' => 01));
         $data=$query->result_array();
    		return  $data;
 	}
+
+    function getCategories()
+    {
+        $query = $this->db->get('categoriesofrestaurant');
+        $data=$query->result_array();
+        return  $data;
+    }
 
 
 	function getAddress($ID)
