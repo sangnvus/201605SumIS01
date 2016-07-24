69653c23e7401647c8b61febc92d5905d496a03f
SE03451 <soulivongse03451@fpt.edu.vn>
2016-07-24 15:23:33 +0700

    sample data

--- References/Query.sql
+++ References/Query.sql
@@ -1,7 +1,5 @@
 -- typeImage: 0 avatar, 1 restaurant image, 2 banner, 3 food
 -- authorityUser: 1 customer, 2 restaurant owner
-
-
 INSERT INTO address(address, provinceID, districtID, wardID) VALUES("18 Ngõ A Đông Tác, Quận Đống Đa", '01', '001', '00001');
 INSERT INTO address(address, provinceID, districtID, wardID) VALUES("20 Ngõ B Đông Tác, Quận Đống Đa", '01', '001', '00001');
 INSERT INTO address(address, provinceID, districtID, wardID) VALUES("32 Ngõ C Đông Tác, Quận Đống Đa", '01', '001', '00001');
@@ -17,16 +15,43 @@ INSERT INTO users(firstNameUser, authorityUser, addressID) VALUES('F', 2, 3);
 INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "http://economictimes.indiatimes.com/thumb/msid-50941625,width-640,resizemode-4/mughal-gardens-in-full-bloom.jpg", 1);
 INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "http://www.gettyimages.co.uk/gi-resources/images/Homepage/Hero/US/embed-504165888.jpg", 2);
 INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTwHCSZ6IF0BqH8ET0DjaSn9ORbY59Zi-NxlLljfKiRV0Ycv1Bj5A", 3);
-INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "http://www.socialmediaexaminer.com/wp-content/uploads/2016/02/kh-social-media-images-600.png", 1);
-INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "https://1.bp.blogspot.com/-pJRvVnLqgt0/VsWYK0K2mRI/AAAAAAAAAFE/W3GPxou4bv8/s1600/holi%2Bimages%2B2.jpg", 2);
+INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "http://www.socialmediaexaminer.com/wp-content/uploads/2016/02/kh-social-media-images-600.png", 4);
+INSERT INTO images(nameImage, typeImage, addressImage, userID) VALUES("A", 1, "https://1.bp.blogspot.com/-pJRvVnLqgt0/VsWYK0K2mRI/AAAAAAAAAFE/W3GPxou4bv8/s1600/holi%2Bimages%2B2.jpg", 5);
 
 INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest A", "Campaign A", 40, 1, 1);
 INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest B", "Campaign B", 60, 2, 2);
 INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest C", "Campaign C", 20, 3, 3);
-INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest D", "Campaign D", 10, 4, 5);
-INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest E", "Campaign E", 10, 3, 6);
+INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest D", "Campaign D", 10, 4, 4);
+INSERT INTO restaurants(nameRe, campaign, discount, addressID, userID) VALUES("Rest E", "Campaign E", 10, 3, 5);
 
 INSERT INTO rate(rateValue, restaurantID, userID) VALUES(4, 1, 1);
 INSERT INTO rate(rateValue, restaurantID, userID) VALUES(5, 2, 2);
 INSERT INTO rate(rateValue, restaurantID, userID) VALUES(1, 3, 3);
 INSERT INTO rate(rateValue, restaurantID, userID) VALUES(2, 4, 4);
+
+-- Categoriesofrestaurant Table
+INSERT INTO categoriesofrestaurant(nameCOR) VALUES('cateA');
+INSERT INTO categoriesofrestaurant(nameCOR) VALUES('cateB');
+INSERT INTO categoriesofrestaurant(nameCOR) VALUES('cateC');
+INSERT INTO categoriesofrestaurant(nameCOR) VALUES('cateD');
+
+-- RestaurantCategoryies Table
+INSERT INTO restaurantcategories VALUES(1, 1);
+INSERT INTO restaurantcategories VALUES(1, 2);
+INSERT INTO restaurantcategories VALUES(1, 3);
+INSERT INTO restaurantcategories VALUES(1, 4);
+
+INSERT INTO restaurantcategories VALUES(2, 1);
+INSERT INTO restaurantcategories VALUES(2, 2);
+INSERT INTO restaurantcategories VALUES(2, 3);
+INSERT INTO restaurantcategories VALUES(2, 4);
+
+INSERT INTO restaurantcategories VALUES(3, 1);
+INSERT INTO restaurantcategories VALUES(3, 2);
+INSERT INTO restaurantcategories VALUES(3, 3);
+INSERT INTO restaurantcategories VALUES(3, 4);
+
+INSERT INTO restaurantcategories VALUES(4, 1);
+INSERT INTO restaurantcategories VALUES(4, 2);
+INSERT INTO restaurantcategories VALUES(4, 3);
+INSERT INTO restaurantcategories VALUES(4, 4);
